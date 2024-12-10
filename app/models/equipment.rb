bd2c692518adc5315cd4d2cb60855b0c33fa6827
HafizRahmat <hafizuddin2001@graduate.utm.my>
2024-12-11 02:52:57 +0800

    Added Validation and Error Messages on Equipment CRUD

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,7 @@
 class Equipment < ApplicationRecord
   self.table_name = "equipments"
+  validates :Equipment_Name, presence: true
+  validates :Financial_Record_Id, presence: true
+  validates :Club_ID, presence: true
+  validates :Vendor_ID, presence: true
 end

b8507056a2977d797db9a1108a5621376b76b7c3
HafizRahmat <hafizuddin2001@graduate.utm.my>
2025-01-06 18:09:34 +0800

    Improve on UI and added associations for Equipments and Vendors

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,11 @@
 class Equipment < ApplicationRecord
   self.table_name = "equipments"
+
   belongs_to :club, foreign_key: "Club_ID"
+  belongs_to :financial_record, foreign_key: "Financial_Record_Id"
+  belongs_to :club, foreign_key: "Club_ID"
+  belongs_to :vendor, foreign_key: "Vendor_ID"
+
   validates :Equipment_Name, presence: true
   validates :Financial_Record_Id, presence: true
   validates :Club_ID, presence: true
